8ee512c74e125e055a32cda2c54d9d925b090a35
Miguel Savignano <migue.masx@gmail.com>
2019-03-20 19:47:03 +0100

    generate swagger json

--- examples/nestjs-tracer-example/src/main.ts
+++ examples/nestjs-tracer-example/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
+  fs.writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
