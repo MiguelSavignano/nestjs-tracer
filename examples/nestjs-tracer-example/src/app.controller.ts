5288484a1e9c33ab5a75cd1de1a439aeb3dac7ea
Miguel Savignano <migue.masx@gmail.com>
2019-03-20 19:45:06 +0100

    add swagger

--- examples/nestjs-tracer-example/src/app.controller.ts
+++ examples/nestjs-tracer-example/src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiImplicitQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiImplicitQuery({
+    name: 'example',
+    description: 'some',
+    required: false,
+    type: 'string',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
