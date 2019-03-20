25e5b7fecbb2bf62db642e9e5b9486fdade1806b
Miguel Savignano <migue.masx@gmail.com>
2019-03-20 19:49:36 +0100

    print log works

--- examples/nestjs-tracer-example/src/app.controller.ts
+++ examples/nestjs-tracer-example/src/app.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiImplicitQuery } from '@nestjs/swagger';
+import { PrintLog } from '../../../src/index';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
   @ApiImplicitQuery({
     name: 'example',
     description: 'some',
     required: false,
     type: 'string',
   })
-  @Get()
+  @PrintLog
   getHello(): string {
     return this.appService.getHello();
   }
