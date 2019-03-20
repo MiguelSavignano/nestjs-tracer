0ffd77a8facdb5a671de077704e35a4e44d3fdd1
Miguel Savignano <migue.masx@gmail.com>
2019-03-21 00:45:45 +0100

    example

--- examples/nestjs-tracer-example/src/app.controller.ts
+++ examples/nestjs-tracer-example/src/app.controller.ts
@@ -1,52 +1,7 @@
 import { Controller, Get, Query, Logger } from '@nestjs/common';
-import { AppService } from './app.service';
-
 import { ApiModelPropertyOptional } from '@nestjs/swagger';
-
-export const PrintLog = function(target, name, descriptor) {
-  const className = target.constructor.name;
-  const original = descriptor.value;
-
-  const handler = {
-    apply: function(target, thisArg, args) {
-      Logger.log(
-        `Call with args: ${JSON.stringify(args)}`,
-        `${className}#${name}`,
-      );
-      const result = target.apply(thisArg, args);
-      Logger.log(`Return: ${JSON.stringify(result)}`, `${className}#${name}`);
-      return result;
-    },
-  };
-  const proxy = new Proxy(original, handler);
-  descriptor.value = proxy;
-};
-
-export const PrintLogAsync = function(target, name, descriptor) {
-  const className = target.constructor.name;
-  const original = descriptor.value;
-
-  const handler = {
-    apply: function(target, thisArg, args) {
-      Logger.log(
-        `Call with args: ${JSON.stringify(args)}`,
-        `${className}#${name}`,
-      );
-      const result = original.apply(thisArg, args);
-      result
-        .then(result => {
-          Logger.log(
-            `Return: ${JSON.stringify(result)}`,
-            `${className}#${name}`,
-          );
-        })
-        .catch(error => {});
-      return result;
-    },
-  };
-  const proxy = new Proxy(original, handler);
-  descriptor.value = proxy;
-};
+import { AppService } from './app.service';
+import { PrintLog, PrintLogAsync } from '../../../src/index';
 
 export class GetHelloDto {
   @ApiModelPropertyOptional()
@@ -60,13 +15,12 @@ export class AppController {
   @Get()
   @PrintLog
   getHelloDto(@Query() input: GetHelloDto): string {
-    // return `hello world ${input.name}`;
-    return this.appService.getHello();
+    return `${this.appService.getHello()} ${input.name}`;
   }
 
   @Get('async')
   @PrintLogAsync
   async getHelloDtoAsync(@Query() input: GetHelloDto): Promise<string> {
-    return `${this.appService.getHello()}-${input.name}`;
+    return `${this.appService.getHello()} ${input.name}`;
   }
 }
