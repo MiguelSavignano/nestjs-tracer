8b355b9d2ad65053387ac5245826075fb108f39a
Miguel Savignano <migue.masx@gmail.com>
2019-04-03 00:45:49 +0200

    add printlog proxy to request Logger

--- src/request-context/index.ts
+++ src/request-context/index.ts
@@ -3,9 +3,11 @@ export { ContextService } from "./ContextService";
 
 import {
   PrintLog as PrintLogCore,
-  PrintLogAsync as PrintLogCoreAsync
+  PrintLogAsync as PrintLogCoreAsync,
+  PrintLogProxy as PrintLogProxyCore
 } from "../PrintLogger";
 
 export const PrintLog = PrintLogCore(RequestLogger);
 export const PrintLogAsync = PrintLogCoreAsync(RequestLogger);
+export const PrintLogProxy = PrintLogProxyCore(RequestLogger);
 export { RequestLogger };
